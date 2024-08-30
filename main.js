cd7b1e8ff93d5e4225941a2927c38f605f8d3e12
gigg88 <popp.christian88@gmail.com>
2024-08-30 09:30:55 +0300

    c

---
+++ main.js
@@ -0,0 +1,22 @@
+//Take a regular function declaration (e.g., function add(a, b) { return a + b; }) and convert it into an arrow function. 
+//Explain the differences in syntax and behavior between the two forms.
+
+function add (a,b) {
+    return a + b;
+}
+
+const add = (a,b) => {
+    return a + b;
+}
+
+
+//Task 1: Pure Function Practice
+
+//Write a pure function named calculateArea that takes two arguments, length, and width, 
+//and returns the area of a rectangle. Explain why your function is considered pure.
+
+function calculateArea (50, 50) {
+    return  length * width;
+}
+
+alert ("calculateArea");
